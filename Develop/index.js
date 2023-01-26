7e934617d6053e768bbe2114ea2b3e617286e03e
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-01-26 15:55:44 +1100

    added questions to the array

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+
+
+// TODO: Create an array of questions for user input
+var q1="Enter the name of your application", 
+    q2="What was your motivation?",
+    q3="Why did you build this project",
+    q4="What problem does it solve?",
+    q5="What did you learn?", 
+    q6="What makes your project stand out?",
+    q7="Describe the challenges in making the app?",
+    q8="Is there any features you plan to implement in the future?",
+    q9="Include screenshots from your application",
+    q10="Please provide the link to the deployed application";
+const questions = [q1,q2,q3,q4,q5,q6,q8,q9,q10];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    //display message:"Please enter the answers to the following questions, 
+    //press enter after each answer if there is no answer just press enter."
+}
+
+// Function call to initialize app
+init();
