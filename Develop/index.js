f164d83f626aff83557e351d07272ee063d752f1
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-01-27 16:13:05 +1100

    modified questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,32 +1,35 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
 const inquirer = require('inquirer');
-
-
 const { writeFile } = require('fs').promises;
 
 
 
 
-// TODO: Create an array of questions for user input
+//Create an array of questions for user input
 
 var q1="Enter the name of your application", 
+
     q2="What was your motivation?",
-    q3="Why did you build this project",
-    q4="What problem does it solve?",
-    q5="What did you learn?", 
-    q6="What makes your project stand out?",
-    q7="Describe the challenges in making the app?",
-    q8="Is there any features you plan to implement in the future?",
-    q9="Include screenshots from your application",
-    q10="Please provide the link to the deployed application";
-const questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+   
+    q3="What problem does it solve?",
+    
+
+    q4="Describe step by step how your app interacts with the user",
+    
+    q5="What makes your project stand out?",
+
+
+    q6="Describe the challenges in making the app?",
+    q7="Is there any features you plan to implement in the future?",
+    q8="Include screenshots from your application",
+    q9="Please provide the link to the deployed application";
+const questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9];
 
 
 
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
 
+//function to display questions and add answers to generatReadme
 const promptUser = () => {
   prompts = []
   for(var i=0; i<10; i++) {
@@ -40,7 +43,7 @@ const promptUser = () => {
   return inquirer.prompt(prompts);
 };
 
-
+//creates a generateReadme function to get the answers from the user and generate the Readme file
 const generateReadme = ({ ans0, ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9 }) =>
   `# ${ans0}
      ${ans1}
@@ -59,7 +62,7 @@ const generateReadme = ({ ans0, ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, 
 //Create a function to initialize app
 const init = () => {
   promptUser()
-  
+    //Create a function to write README file
     .then((answers) => writeFile('README.md', generateReadme(answers)))
     .then(() => console.log('Successfully wrote to README.md'))
     .catch((err) => console.error(err));
